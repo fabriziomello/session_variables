42888ac564733cb9735352664f24ac978f179842
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2012-11-27 00:10:19 -0200

    Added tests

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -2,7 +2,10 @@
 BEGIN;
 \i sql/session_variables.sql
 \set ECHO all
-
--- Tests goes here.
-
+SELECT set_value('key', 'value');
+SELECT set_value('Key', 'Value');
+SELECT set_value('KEY', 'VALUE');
+SELECT get_value('key');
+SELECT get_value('Key');
+SELECT get_value('KEY');
 ROLLBACK;
